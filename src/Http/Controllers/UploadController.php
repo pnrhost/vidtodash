550709116bef6d991ce8942e0005f7a2e461b11f
reza <privyreza@gmail.com>
2017-09-13 17:30:39 +0200

    first commit

---
+++ src/Http/Controllers/UploadController.php
@@ -0,0 +1,2 @@
+<?php
+namespace Pnrhost\Vidstodash\Http\Controllers;
---
+++ src/VidtodashServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pnrhost\Vidtodash;
+
+use Illuminate\Support\ServiceProvider;
+
+class VidtodashServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
